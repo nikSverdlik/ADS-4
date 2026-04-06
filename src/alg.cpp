4535cedfb8a80b34ba78640728505551e6c82dff
nikSverdlik <niksverdlik@gmail.com>
2026-04-06 03:49:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,13 @@ int countPairs1(int *arr, int len, int value) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 res++;
+                while (j + 1 < len && arr[j] == arr[j + 1]) {
+                    j++;
+                }
             }
+        }
+        while (i + 1 < len && arr[i] == arr[i + 1]) {
+            i++;
         }
     }
     return res;
